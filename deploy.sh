47f5df2d4dba24c7b6ecae73c8558a59dbcc2d30
Thomas Belin <ThomasBelin4@gmail.com>
2013-07-06 15:46:08 +0200

    add auto git action for deploying

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
+git checkout master
+git merge develop
+git tag deploy
+git push
 cd public
 make
 cd -
 af update blog-thomasbelin
-phantomjs scripts/snapshots.js
+#phantomjs scripts/snapshots.js
+git checkout develop
